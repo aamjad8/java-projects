08a7973dc7d7cedcaa11dd74239ba57d38f603f2
Ayesha Amjad <ayesha@ayeshas-laptop.mynetworksettings.com>
2024-07-08 17:39:27 -0400

    My work

--- src/com/tutor/lesson/ayesha/HomeWorkOne.java
+++ src/com/tutor/lesson/ayesha/HomeWorkOne.java
@@ -4,7 +4,8 @@ public class HomeWorkOne {
     public static void main(String[] args){
         subtraction(289, 756);
         multiplication(56, 87);
-        division(97, 74);
+        division(97.4f, 74);
+        remainder(97, 74);
     }
     static void subtraction(int num1, int num2){
         int difference = num1 - num2;
@@ -14,8 +15,12 @@ public class HomeWorkOne {
         int product = num3 * num4;
         System.out.println("The product of " + num3 + " and " + num4 + " is " + product);
     }
-    static void division(int num5, int num6){
+    static void division(float num5, float num6){
         double quotient = num5/num6;
         System.out.println("The quotient of " + num5 + " and " + num6 + " is " + quotient);
+    }
+    static void remainder(int num5, int num6){
+        double quotient = num5%num6;
+        System.out.println("The remainder of " + num5 + " and " + num6 + " is " + quotient);
     }
 }
